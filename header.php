813803606bf764ca53ff0d770d5791325473af29
Reece Crowther <reececrowther@hotmail.co.uk>
2023-03-02 15:33:44 +0000

    Header Change

--- header.php
+++ header.php
@@ -27,22 +27,7 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$deliver_test_description = get_bloginfo( 'description', 'display' );
-			if ( $deliver_test_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $deliver_test_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			Deliver Media Testing Site Logo
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
